8de9164b3def26d20b8e2c040f664f7271bc5822
Luke Rocco <luke.rocco@sixcap.tech>
2017-09-22 15:53:01 +0200

    Added careers page without career details

---
+++ pages/careers/careers.const.js
@@ -0,0 +1,89 @@
+export const CAREERS = [
+  {
+    name: 'AI Engineer',
+    sector: 'Data Science',
+    link: 'ai-engineer',
+    description: 'As part of the AI team you will be a key member in proposing and developing the AI solutions within amico.ai and will be reporting to our Lead AI Data Scientist.',
+    responsibilities: [
+      'Have a pulse on new technologies and a desire to change the way business gets done',
+      'Implement emerging solutions',
+      'Develop applications using leading AI technology frameworks',
+      'Undertakes complex projects requiring additional specialised technical knowledge',
+      'Makes decisions on complex or ambiguous architecture issues',
+      'Coordinates with users to determine requirements',
+      'Ensures that system improvements are successfully implemented and monitored to increase efficiency'
+    ],
+    requirements: [
+      'Minimum 2 years of hands on web application development experience',
+      'Bachelor degree in Computer Science',
+      'Basic understanding and experience with machine learning fundamentals and analysis tools',
+      'Strong analytical skills, good judgement and passion for AI',
+      'Must be awesome and curious',
+      'Must have a positive attitude, be a team player and be open to receiving constructive criticism'
+    ],
+    skills: null,
+    preferredSkills: null,
+    qualifications: null
+  },
+  {
+    name: 'Android Developer',
+    sector: 'Native App Development',
+    link: 'android-developer',
+    description: null,
+    responsibilities: [
+      'Write clean and testable code',
+      'Write unit and instrumentation tests',
+      'Use REST APIs'
+    ],
+    requirements: [
+      'Someone who lives and breathes anything Android',
+      'Interest in mobile UI and UX',
+      'Experience using CI / CD tools such as BuddyBuild, CircleCi, Jenkins',
+      'Knowledgeable in Architecture Patterns such as MVP, MVVM, Clean Architecture',
+      'Deep understanding of the Android SDK, Support library and the rest of the framework',
+      'Experience with libraries such as Dagger, Retrofit',
+      'Extensive knowledge of Java & Kotlin',
+      'Initiative to develop an app from start to finish, including publishing and support',
+      'Keep tabs on new developments in the Android community',
+      'Eager to learn and self-motivated',
+      'Someone who tinkers with different options before settling on a solution'
+    ],
+    skills: null,
+    preferredSkills: null,
+    qualifications: null
+  },
+  {
+    name: 'Front-End Developer',
+    sector: 'Web Development',
+    link: 'frontend-developer',
+    description: null,
+    responsibilities: [
+      'Contribute directly to the success of our products by materialising our product teams’ vision into a tangible web experience',
+      'Be responsible for front-end development in a scrum team using lightweight agile processes',
+      'Participate in sprint planning, task estimation, development, testing, code reviews, bug fixing, deploying great software, implementing new features as well as maintaining and improving the existing functionality'
+    ],
+    requirements: [
+      'Strong analytical skills and passion for work',
+      'A positive attitude and able to work in a team',
+      'Previous experience as a front-end developer in an agile environment',
+      'Genuine interest and familiarity with building interfaces for great web experiences'
+    ],
+    skills: [
+      'JS framework for single page apps including Vue, React and Aurelia',
+      'ES2015 / ES6',
+      'Stateless REST APIs',
+      'Scalable and modular styles using SASS or LESS',
+      'Building mobile-first/responsive sites/apps',
+      'Git and version control',
+      'Redux state management pattern'
+    ],
+    preferredSkills: [
+      'Vue JS, Vue Router and Vuex - the framework we are using for our front-end projects',
+      'Progressive Web Apps',
+      'TDD with strong knowledge of Unit and E2E testing',
+      'WebGL',
+      'Webpack'
+    ],
+    qualifications: null
+  }
+]
--- pages/careers/index.vue
+++ pages/careers/index.vue
@@ -1,5 +1,213 @@
 <template>
-  <section class="a-page-wrapper">
-    <h1>CAREERS</h1>
-  </section>
+  <article class="a-content">
+
+    <!-- CAREERS VIEW -->
+    <section v-if="!isDetailsRouteVisible" class="a-careers">
+      <!-- INTRO SECTION -->
+      <div class="a-careers-intro">
+        <div class="contained-content">
+
+          <h2 class="a-intro-title">Come and join us!</h2>
+
+          <!-- COMPANY VALUES MOBILE -->
+          <div v-if="isMobile" class="a-company-values">
+            <carousel
+              :perPage="1"
+              :perPageCustom="[[768, 2]]"
+              :minSwipeDistance="30"
+              :scrollPerPage="true"
+              :navigationEnabled="false"
+              :paginationEnabled="true"
+              paginationColor="#008ae5"
+              paginationActiveColor="#ffffff">
+
+              <!-- FULFILMENT -->
+              <slide>
+                <div class="a-company-value">
+                  <div class="a-value-header">
+                    <img class="a-value-icon" alt="Career Fulfilment illustration" src="~/static/images/career-fulfilment.svg">
+                    <h5 class="a-value-title">Fulfilment</h5>
+                  </div>
+                  <div class="a-value-content">
+                    <p class="a-value-body small">
+                      We are building a super talented team. Everyone here
+                      holds each other to such high standards, that we end
+                      up challenging ourselves.
+                    </p>
+                    <p class="a-value-body small">
+                      As we work, we are not just developing products but
+                      principally ourselves as professionals. And so after
+                      a day at work we go back home with our heads held
+                      high with pride.
+                    </p>
+                  </div>
+                </div>
+              </slide>
+
+              <!-- PURPOSE -->
+              <slide>
+                <div class="a-company-value">
+                  <div class="a-value-header">
+                    <img class="a-value-icon" alt="Career Purpose illustration" src="~/static/images/career-purpose.svg" style="padding-left: 22px;">
+                    <h5 class="a-value-title">Purpose</h5>
+                  </div>
+                  <div class="a-value-content">
+                    <p class="a-value-body small">
+                      Our every-day motivation comes from having a purpose that
+                      is larger than ourselves.
+                    </p>
+                    <p class="a-value-body small">
+                      We are working to achieve breakthroughs in Adaptive AI, a groundbreaking technology that is already transforming the future and will create lots of positive value in the world.
+                    </p>
+                  </div>
+                </div>
+              </slide>
+
+              <!-- BENEFITS -->
+              <slide>
+                <div class="a-company-value">
+                  <div class="a-value-header">
+                      <img class="a-value-icon" alt="Career Benefits illustration" src="~/static/images/career-benefits.svg">
+                    <h5 class="a-value-title">Benefits</h5>
+                  </div>
+                  <div class="a-value-content">
+                    <p class="a-value-body small">
+                      Working in a friendly and relaxed environment makes our productivity grow.
+                    </p>
+                    <p class="a-value-body small">
+                      We offer flexible hours, competitive salaries, social events, a personal training fund, use of swimming pool, games room, free drinks, snacks and more. This is the true revenge of the nerds.
+                    </p>
+                  </div>
+                </div>
+              </slide>
+            </carousel>
+          </div>
+
+          <!-- COMPANY VALUES DESKTOP -->
+          <div v-else class="a-company-values">
+            <!-- FULFILMENT -->
+            <div class="a-company-value">
+              <div class="a-value-wrapper">
+                <div class="a-value-header">
+                    <img class="a-value-icon" alt="Career Fulfilment illustration" src="~/static/images/career-fulfilment.svg">
+                  <h5 class="a-value-title">Fulfilment</h5>
+                </div>
+                <div class="a-value-content">
+                  <p class="a-value-body small">
+                    We are building a super talented team. Everyone here
+                    holds each other to such high standards, that we end
+                    up challenging ourselves.
+                  </p>
+                  <p class="a-value-body small">
+                    As we work, we are not just developing products but
+                    principally ourselves as professionals. And so after
+                    a day at work we go back home with our heads held
+                    high with pride.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <!-- PURPOSE -->
+            <div class="a-company-value">
+              <div class="a-value-wrapper">
+                <div class="a-value-header">
+                  <img class="a-value-icon" alt="Career Purpose illustration" src="~/static/images/career-purpose.svg" style="padding-left: 22px;">
+                  <h5 class="a-value-title">Purpose</h5>
+                </div>
+                <div class="a-value-content">
+                  <p class="a-value-body small">
+                    Our every-day motivation comes from having a purpose that
+                    is larger than ourselves.
+                  </p>
+                  <p class="a-value-body small">
+                    We are working to achieve breakthroughs in Adaptive AI, a groundbreaking technology that is already transforming the future and will create lots of positive value in the world.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <!-- BENEFITS -->
+            <div class="a-company-value">
+              <div class="a-value-wrapper">
+                <div class="a-value-header">
+                  <img class="a-value-icon" alt="Career Benefits illustration" src="~/static/images/career-benefits.svg">
+                  <h5 class="a-value-title">Benefits</h5>
+                </div>
+                <div class="a-value-content">
+                  <p class="a-value-body small">
+                    Working in a friendly and relaxed environment makes our productivity grow.
+                  </p>
+                  <p class="a-value-body small">
+                    We offer flexible hours, competitive salaries, social events, a personal training fund, use of swimming pool, games room, free drinks, snacks and more. This is the true revenge of the nerds.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+      <!-- CAREER OPENINGS -->
+      <div class="content alternate-content a-career-openings">
+        <div class="contained-content">
+          <div class="content-header">
+            <h3 class="a-content-title">Openings</h3>
+            <hr/>
+          </div>
+          <div class="a-openings">
+            <div
+              v-for="career in CAREERS" :key="career.name"
+              class="a-opening">
+
+              <nuxt-link
+                :to="{name: 'career-details', params: {link: career.link, career: career, isDetailsRouteVisible: true}}" class="a-opening-link"
+                @click.native="isDetailsRouteVisible = true">
+                <p class="strong">{{ career.name }}</p>
+                <p class="x-small">{{ career.sector }}</p>
+              </nuxt-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- CAREER DETAILS SUB VIEW -->
+    <!-- <transition name="fade-in">
+      <section v-if="isDetailsRouteVisible" class="a-career-details">
+        <router-view></router-view>
+      </section>
+    </transition> -->
+
+  </article>
 </template>
+
+<style lang="scss" src="./careers.scss"></style>
+
+<script>
+import { CAREERS } from './careers.const'
+import { Slide, Carousel } from 'vue-carousel'
+
+export default {
+  components: {
+    Slide,
+    Carousel
+  },
+  data () {
+    return {
+      CAREERS,
+      isMobile: true,
+      isDetailsRouteVisible: false
+    }
+  },
+  mounted () {
+    if (window.innerWidth >= 920) {
+      this.isMobile = false
+    }
+  },
+  updated () {
+    this.isDetailsRouteVisible = this.$route.params.isDetailsRouteVisible
+  }
+}
+</script>
